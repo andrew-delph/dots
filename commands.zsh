e2a93f5932946a7a7c65f92b6aeb37068a9dc2d4
Andrew Delph <andrewdelph@live.ca>
2025-01-05 12:22:31 -0800

    zsh wip

---
+++ commands.zsh
@@ -0,0 +1,9 @@
+
+alias editz="nvim ~/.zshrc"
+alias edit="nvim ~/dots/zsh"
+alias envim="nvim ~/dots/nvim"
+alias refresh="omz reload"
+
+alias tk='tmux kill-session'
+alias ta='tmux attach-session -t'
+alias tl='tmux list-sessions'
