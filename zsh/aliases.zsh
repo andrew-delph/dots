64b785c47342cd7ab2351f21c6f08673a7b30c8f
Andrew Delph <andrewdelph@live.ca>
2025-01-05 12:55:43 -0800

    alias.zsh init.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,5 +5,6 @@ alias envim="nvim ~/dots/nvim"
 alias refresh="omz reload"
 
 alias tk='tmux kill-session'
+alias tkk='tmux kill-session -t'
 alias ta='tmux attach-session -t'
 alias tl='tmux list-sessions'
---
+++ zsh/init.zsh
@@ -0,0 +1,3 @@
+
+echo "### tmux sessions"
+tl
