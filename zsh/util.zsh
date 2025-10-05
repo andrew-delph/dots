b2771fcf5334867f54553ac8ace161398bbdc573
Andrew Delph <andrewdelph@live.ca>
2025-10-05 12:47:18 -0700

    Add Q CLI aliases for chat commands

--- zsh/util.zsh
+++ zsh/util.zsh
@@ -14,3 +14,6 @@ alias tkk='tmux kill-session -t'
 alias ta='tmux attach-session -t'
 alias tl='tmux list-sessions'
 
+alias qe="q chat --trust-all-tools --no-interactive"
+
+alias gcai="q chat --trust-all-tools --no-interactive 'Commit all changes in the current Git repository with a commit message specific to the changes.'"
